1dabea89ef46d6d7a49d0af84c51617a49469e40
Antal Áron <antalaron@antalaron.hu>
2018-02-16 11:53:55 +0100

    Minor fix typo childs -> children

--- jquery.ggergoMillerColumns.js
+++ jquery.ggergoMillerColumns.js
@@ -6,7 +6,7 @@
             root            :   1,
             column          :   'ul',
             option          :   'li',
-            childs          :   function (id) {},
+            children        :   function (id) {},
             optionContent   :   function (id) {},
             optionAttr      :   function (id) { return { 'tabindex': '1' }; },
             columnAttr      :   function (id) { return {}; },
@@ -25,54 +25,54 @@
                                 0 < $(event.target).prev('*[data-parentId]').children('*[data-id]').length      ? $(event.target).prev('*[data-parentId]').children('*[class~="selected"]') :
                                 0 < $('#'+this.attr('id')+' *[class~="selected"]').length                       ? $('#'+this.attr('id')+' *[class~="selected"]').last() :
                                 $('#'+this.attr('id')+' *[data-id]:first-child');
-                
-                if (this.data('ggergo-mc').childs.call(this, option.attr('data-id')).length > 0)
+
+                if (this.data('ggergo-mc').children.call(this, option.attr('data-id')).length > 0)
                 {
                     event.preventDefault();
                 }
-                
+
                 // return option for selecting node
                 return option;
             }
         };
-    
-    var showChilds = function (id, childs)
+
+    var showChildren = function (id, children)
     {
-        if (childs.length > 0)
+        if (children.length > 0)
         {
             var $this = this;
             var column = $('<'+this.data('ggergo-mc').column+'/>', this.data('ggergo-mc').columnAttr.call(this,id))
                                 .attr('data-parentId', id)
-            
-            for(var i = 0; i < childs.length; i++)
+
+            for(var i = 0; i < children.length; i++)
             {
-                var option = $('<'+$this.data('ggergo-mc').option+'/>', $this.data('ggergo-mc').optionAttr.call(this,childs[i]))
-                                .attr('data-id', childs[i])
-                                .html($this.data('ggergo-mc').optionContent.call($this,childs[i]));
+                var option = $('<'+$this.data('ggergo-mc').option+'/>', $this.data('ggergo-mc').optionAttr.call(this,children[i]))
+                                .attr('data-id', children[i])
+                                .html($this.data('ggergo-mc').optionContent.call($this,children[i]));
                 column.append(option);
             }
-            
+
             this.append(column);
         }
     }
-    
+
     var methods =
     {
         init : function( parameters )
         {
             if (typeof this.attr('id') === 'undefined' || this.attr('id') === '') { $.error('Selected element must have id attribute!') }
-            
+
             var $this = this;
             if ( typeof(this.data('ggergo-mc')) === 'undefined' )
             {
                 // merge settings
                 this.data('ggergo-mc', $.extend({}, defaults, parameters));
-                
+
                 // attach click event listener
                 $(document).on('click', '#'+this.attr('id'), function (event)
                 {
                     var option = $this.data('ggergo-mc').onClick.call($(this), event);
-                    
+
                     if (typeof option !== 'undefined' && option !== null)
                     {
                         methods.selectNode.call($this, option);
@@ -84,16 +84,16 @@
                 // merge settings
                 this.data('ggergo-mc', $.extend({}, this.data('ggergo-mc'), parameters));
             }
-            
+
             // select root node
             this.html('');
             methods.selectNode.call(this, this.data('ggergo-mc').root);
         },
-        
+
         selectNode : function (option)
         {
             var id;
-            
+
             // existing
             if (typeof option === 'object')
             {
@@ -101,10 +101,10 @@
                 {
                     $('#'+this.attr('id')+' *[data-parentId]:last').remove();
                 }
-                
+
                 option.parent().children('*[data-id]').removeClass('selected');
                 option.addClass('selected');
-                
+
                 id = option.attr('data-id');
             }
             // non existent
@@ -113,18 +113,18 @@
                 id = option;
                 option = $('#'+this.attr('id')+' *[data-id="'+id+'"]').last();
             }
-            
-            showChilds.call(this, id, this.data('ggergo-mc').childs.call(this, id));
-            
+
+            showChildren.call(this, id, this.data('ggergo-mc').children.call(this, id));
+
             this.data('ggergo-mc').selectCallback.call(this,option);
         }
     };
-    
+
     $.fn[pluginName] = function(method)
     {
         return  methods[method]                         ? methods[method].apply( this, Array.prototype.slice.call( arguments, 1 )) :
                 typeof method === 'object' || ! method  ? methods.init.apply( this, arguments ) :
                 $.error( 'Method ' +  method + ' does not exist on jQuery.' + pluginName );
     };
-    
+
 })( jQuery );
